bc49efbc157cdc654162888ea3fc9d5696e211c4
renardbergson <renard.contato@gmail.com>
2026-01-21 16:54:37 -0300

    feat:add product routing

--- src/main/java/com/ecommerce/ProductController.java
+++ src/main/java/com/ecommerce/ProductController.java
@@ -3,12 +3,21 @@ package com.ecommerce;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
   @GetMapping("/product")
   public String product(Model model) {
     model.addAttribute("adminSection", "product");
+    model.addAttribute("product", new Product());
     return "admin";
+  }
+
+  @PostMapping("/product/save")
+  public String saveProduct(@ModelAttribute Product product) {
+    System.out.println("Product saved: " + product);
+    return "redirect:/product";
   }
 }
