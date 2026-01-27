0c0577b3b0c8314487e09b10237619d1f88e0bdc
renardbergson <renard.contato@gmail.com>
2026-01-26 22:14:58 -0300

    feat: adding signup routes and it's features

--- src/main/java/com/ecommerce/LoginController.java
+++ src/main/java/com/ecommerce/LoginController.java
@@ -1,17 +1,21 @@
 package com.ecommerce;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class Login {
+public class LoginController {
   @GetMapping("/signin")
   public String signin() {
     return "signin";
   }
 
   @GetMapping("/signup")
-  public String signup() {
+  public String signup(Model model) {
+    model.addAttribute("user", new User());
     return "signup";
   }
 
--- src/main/java/com/ecommerce/UserController.java
+++ src/main/java/com/ecommerce/UserController.java
@@ -1,14 +1,29 @@
 package com.ecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+  @Autowired
+  private UserRepository userRepository;
+
   @GetMapping("/user")
   public String user(Model model) {
     model.addAttribute("adminSection", "user");
     return "admin";
+  }
+
+  @PostMapping("/signup")
+  public String signup(@ModelAttribute User user) {
+    System.out.println("Attempting to create user: " + user);
+    // set o atributo "status" e retorna a instância já atualizada
+    userRepository.save(user.active());
+    System.out.println("User created successfully: " + user);
+    return "redirect:/signin";
   }
 }
